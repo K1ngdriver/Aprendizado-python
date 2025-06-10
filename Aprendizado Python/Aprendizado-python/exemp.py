f6f837cf4eba6e6dc303e99e47cb73b24defb32b
Lucas Barbosa <lucas.oli.barb@gmail.com>
2025-06-10 00:37:37 -0300

    algumas funções simples como exemplo

--- Aprendizado Python/Aprendizado-python/exemp.py
+++ Aprendizado Python/Aprendizado-python/exemp.py
@@ -9,3 +9,46 @@ elif x == 1:
     print('Single')
 else:
     print('More')
+
+#While em python
+# Contagem regressiva:
+contador = 5
+while contador > 0:
+    print(contador)
+    contador -= 1
+
+#For em python
+# Mede algumas strings:
+palavras = ['gato', 'janela', 'defenestrar']
+for p in palavras:
+    print(p, len(p))
+
+# Cria uma amostra de coleção
+users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+
+# Estratégia: iterar por uma cópia
+for user, status in users.copy().items():
+    if status == 'inactive':
+        del users[user]
+
+# Estratégia: criar uma nova coleção
+active_users = {}
+for user, status in users.items():
+    if status == 'active':
+        active_users[user] = status
+
+#Range em python
+for i in range(5):
+    print(i)
+
+list(range(5, 10))
+
+
+list(range(0, 10, 3))
+
+
+list(range(-10, -100, -30))
+
+a = ['Maria', 'tinha', 'um', 'carneirinho']
+for i in range(len(a)):
+    print(i, a[i])
